75341f26b0e482a8618f00126b3ababc7093a1cf
MrMubaroq <mubaroqahmad89@gmail.com>
2024-11-05 10:45:38 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function cekGanjilGenap(angka) {
+    if (angka % 2 == 0) {
+      console.log(`Angka ini ${angka} adalah genap`);
+    } else {
+      console.log(`Angka ini ${angka} adalah ganjil`);
+    }
+  }
+  
+  cekGanjilGenap(9);
+  cekGanjilGenap(6);
